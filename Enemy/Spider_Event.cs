fa14cb15bc7a517b8ea2c7c226b2e9f82555d57a
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-13 21:58:44 -0400

    Testing that function can be called from animator, has to be attached to the object with the animator component

--- Enemy/Spider_Event.cs
+++ Enemy/Spider_Event.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Spider_Event : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Fire()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //tell spider to fire
+        Debug.Log("Spider should fire");
     }
 }
