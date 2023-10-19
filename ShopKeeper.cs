102fb257692683c6f90c5b02c671fdde8254b4d4
Julia Grimes <julia.s.grimes@gmail.com>
2023-10-19 18:18:36 -0400

    Create shopkeeper script

---
+++ ShopKeeper.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeeper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Entered Shop");
+        }
+    }
+}
